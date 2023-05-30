fe5efb13eb647b7506e580b8307097916e486efe
Austin <105258719+Austin-540@users.noreply.github.com>
2023-05-30 18:14:08 +1200

    its working now :)

--- add_data_to_db.py
+++ add_data_to_db.py
@@ -20,13 +20,52 @@ homeroom = "00EXM" #For now, because my CSV file doesn't have homerooms yet
 
 
 def uploadLesson(listOfDetails):
+    students_in_lesson = []
     time = listOfDetails[0]
     for j in range(len(listOfDetails)):
+        students_in_lesson.append(listOfDetails[j])
         if j == 0:
             pass
         elif listOfDetails[j] != '':
-            # if students_already_in_DB[0]
-            client.collection('students').create({"name": listOfDetails[j]});
+            if listOfDetails[j] in list_of_names_in_DB:
+                pass
+            else:
+                client.collection('students').create({"name": listOfDetails[j], "homeroom":homeroom});
+    
+    """
+    Required Data:
+    teacher id -done
+    instrument -done
+    students db ids -done
+    weekday -done
+    time -done
+    """
+
+   
+    all_teachers = client.collection("users").get_full_list() 
+    #It is supposedly possible to use the get_one method with a filter to accomplish this, but it was giving me rediculously unhelpful error messages
+    print(all_teachers)
+    for x in range(len(all_teachers)):
+        if all_teachers[x].collection_id['username'] == teacher:
+            teacher_id = all_teachers[x].collection_id['id']
+
+    all_students = client.collection("students").get_full_list()
+    student_db_IDs = []
+    for student in students_in_lesson:
+        for x in range(len(all_students)):
+            if all_students[x].collection_id['name'] == student:
+                print(all_students[x].collection_id['name'])
+                student_db_IDs.append(all_students[x].collection_id['id'])
+    
+    client.collection('lessons').create(
+        {
+            'teacher': teacher_id,
+            'instrument': instrument,
+            'students': student_db_IDs,
+            'weekday' : date,
+            'time': time
+        }
+    )
 
 
 with open("./shorter_csv.csv") as csv_file:
@@ -37,7 +76,6 @@ with open("./shorter_csv.csv") as csv_file:
     instrument = data[0][0]
     teacher = data[1][0]
     date = data[2][0]
-    for i in range(len(data)):
-        # uploadLesson(data[i])
-        pass
+    for i in range(4, len(data)):
+        uploadLesson(data[i])
     
