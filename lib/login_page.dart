217759976d8ac87283934d1f075e4a1fd4597e1d
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-02 19:29:25 +1200

    Turns out I was using the future builder wrong

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,42 +5,32 @@ import 'globals.dart';
 import 'main.dart';
 import 'package:restart_app/restart_app.dart';
 
-
-
 class LoginScreen extends StatefulWidget {
-
   @override
-  _LoginScreenState createState() => _LoginScreenState(); }
+  _LoginScreenState createState() => _LoginScreenState();
+}
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   String _email = "";
   String _password = "";
-  
-
-  
-  
-
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Login")
-        ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Login")),
       body: Form(
           key: _formKey,
           child: Padding(
             padding: const EdgeInsets.all(50.0),
             child: Column(
-              
               children: [
                 TextFormField(
                   decoration: InputDecoration(
                     labelText: 'Email',
                   ),
-                  
                   onSaved: (value) {
                     _email = value!;
                   },
@@ -54,18 +44,17 @@ class _LoginScreenState extends State<LoginScreen> {
                   onSaved: (value) {
                     _password = value!;
                   },
-                  
                 ),
                 SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
                     _formKey.currentState!.save();
                     if (_email != "" && _password != "") {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => ConfirmLoginPage(email: _email, password: _password)));
-
-
-                      
-                        
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ConfirmLoginPage(
+                                  email: _email, password: _password)));
                     }
                   },
                   child: Text('Login'),
@@ -77,34 +66,31 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-
 class ConfirmLoginPage extends StatelessWidget {
-  const ConfirmLoginPage({super.key, required this.email, required this.password});
+  const ConfirmLoginPage(
+      {super.key, required this.email, required this.password});
   final String email;
   final String password;
 
-
-
   Future logIn(email, password) async {
     try {
+      await Future.delayed(
+          Duration(milliseconds: 500)); //To make testing easier
+      //* Remember to remove before completing project
 
-    await Future.delayed(Duration(milliseconds: 500)); //To make testing easier
-    //* Remember to remove before completing project
+      final authData = await pb.collection('users').authWithPassword(
+            email,
+            password,
+          );
 
-    final authData = await pb.collection('users').authWithPassword(
-  email, password,);
-
-
-  final storage = new FlutterSecureStorage();
-  await storage.write(key: "email", value: email);
-  await storage.write(key: "password", value: password);
-  return authData;
+      final storage = new FlutterSecureStorage();
+      await storage.write(key: "email", value: email);
+      await storage.write(key: "password", value: password);
+      return authData;
     } catch (e) {
       return "Fail";
     }
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -113,9 +99,11 @@ class ConfirmLoginPage extends StatelessWidget {
       initialData: null,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.hasData) {
-          return Placeholder();
+          return MyHomePage(title: "Home");
         } else {
-          return CircularProgressIndicator();
+          return Scaffold(
+            appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: Text("Loading"),),
+            body: Center(child: CircularProgressIndicator()));
         }
       },
     );
