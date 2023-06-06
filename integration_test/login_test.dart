7264cdfb2aa2aeb0d406f83ce8a507a6693eba19
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-06 21:19:32 +1200

    make integration test try logging in

--- integration_test/login_test.dart
+++ integration_test/login_test.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:music_lessons_attendance/main.dart' as app;
 
 void main() {
+
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Login Test - Looking for failed login', () {
     testWidgets('Try to login with bad details',
         (tester) async {
+        final storage = new FlutterSecureStorage();
+        await storage.deleteAll();
       app.main();
       await tester.pumpAndSettle();
 
@@ -19,9 +22,6 @@ void main() {
   
       //Expect nothing to happen if login triggered after doing nothing
 
-
-
-      // await tester.tap(find.bySemanticsLabel("Email Field"));
 
       expect(find.byType(TextFormField), findsNWidgets(2));
 
@@ -35,6 +35,26 @@ void main() {
 
       expect(find.text("Something went wrong"), findsOneWidget);
 
+
+
+
+      await tester.tap(find.byType(ElevatedButton).first);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(TextFormField), findsNWidgets(2));
+
+      await tester.enterText(find.byType(TextFormField).first, "example@example.com");
+      await tester.enterText(find.byType(TextFormField).last, "Password1");
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(find.bySemanticsLabel("Login Button"));
+      
+      await tester.pumpAndSettle();
+
+      print(find.byType(ElevatedButton).toString());
+      expect(find.textContaining("AustinS"), findsOneWidget);
+
+      
     });
   });
 }
