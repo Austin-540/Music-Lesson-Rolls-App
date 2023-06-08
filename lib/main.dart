9b392fdb43e42f13d97dc821754b56ab66b3fb0b
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-08 20:27:06 +1200

    thats embarrasing...

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Music Lesson Attendance',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 134, 193,
                 234)), //"Oxford Blue" according to the communications part of adminitstration
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
