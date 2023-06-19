519da111f4a91487c039e6d2661e705fb5ed566d
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-19 19:58:18 +1200

    unable to go back after submitting

--- lib/marking_the_roll_page.dart
+++ lib/marking_the_roll_page.dart
@@ -109,7 +109,7 @@ class ConfirmPage extends StatelessWidget {
           child: Placeholder()),
           Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Placeholder())), child: Text("Confrim")),
+          child: ElevatedButton(onPressed: () => Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => Placeholder()), (context) => false), child: Text("Confrim")),
         ),
       ]),
     );
