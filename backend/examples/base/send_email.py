19bb940c5ba32bbe4234d74059500c2265f81184
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-20 15:18:56 +1200

    code to send email

--- backend/examples/base/send_email.py
+++ backend/examples/base/send_email.py
@@ -2,15 +2,21 @@
 
 import sqlite3
 from secrets import getSecrets
+import smtplib, ssl
+
 
 secrets = getSecrets()
 
-con = sqlite3.connect("./pb_data/data.db")
+con = sqlite3.connect("/Users/austin/Programming/music_lessons_attendance/backend/examples/base/pb_data/data.db")
 cur = con.cursor()
 
 cur.execute("SELECT * FROM rolls WHERE final = true")
-x = cur.fetchall
-if x.length == 0:
+x = cur.fetchall()
+if len(x) == 0:
     quit()
 
-
+def sendEmail(contents):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(secrets['my_email'], secrets['my_password'])
+        server.sendmail(secrets['my_email'], secrets['reciever_email'], contents)
