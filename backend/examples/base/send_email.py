1185c027c49cc8b58f244cd9b71545eef9eb3200
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-20 15:09:53 +1200

    import secrets

--- backend/examples/base/send_email.py
+++ backend/examples/base/send_email.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 import sqlite3
+from secrets import getSecrets
+
+secrets = getSecrets()
 
 con = sqlite3.connect("./pb_data/data.db")
 cur = con.cursor()
@@ -9,4 +12,5 @@ cur.execute("SELECT * FROM rolls WHERE final = true")
 x = cur.fetchall
 if x.length == 0:
     quit()
+
 
