f458cd82566e0a9b0e61d3b08d58abc776b2b641
Austin <105258719+Austin-540@users.noreply.github.com>
2023-06-20 15:05:11 +1200

    quit the python if final hasn't been recieved

--- backend/examples/base/send_email.py
+++ backend/examples/base/send_email.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
-print("Hello World")
+
+import sqlite3
+
+con = sqlite3.connect("./pb_data/data.db")
+cur = con.cursor()
+
+cur.execute("SELECT * FROM rolls WHERE final = true")
+x = cur.fetchall
+if x.length == 0:
+    quit()
+
