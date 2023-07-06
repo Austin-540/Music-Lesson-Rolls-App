9977fcbca9be4c1ddf2772d5b572e06e804e2a2a
Austin <105258719+Austin-540@users.noreply.github.com>
2023-07-06 21:22:13 +1200

    add email error message

--- backend/examples/base/send_email.py
+++ backend/examples/base/send_email.py
@@ -6,18 +6,6 @@ import smtplib, ssl
 from tabulate import tabulate
 from datetime import datetime
 
-
-secrets = getSecrets()
-
-con = sqlite3.connect("/Users/austin/Programming/music_lessons_attendance/backend/examples/base/pb_data/data.db")
-cur = con.cursor()
-
-cur.execute("SELECT * FROM rolls WHERE final = true")
-x = cur.fetchall()
-if len(x) == 0:
-    con.close()
-    quit()
-
 def sendEmail(contents):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
@@ -37,18 +25,32 @@ def getStudentDetails(student):
     studentDBDetails = cur.execute(f"SELECT name, homeroom FROM students WHERE id = '{student[4]}'").fetchone()
     return [studentDBDetails[0], studentDBDetails[1], status]
 
-    
 
-allDetails = []
-allRolls = cur.execute("SELECT * FROM rolls").fetchall()
-for student in allRolls:
-    x = getStudentDetails(student)
-    allDetails.append(x)
+secrets = getSecrets()
+
+con = sqlite3.connect("/Users/austin/Programming/music_lessons_attendance/backend/examples/base/pb_data/data.db")
+cur = con.cursor()
+
+cur.execute("SELECT * FROM rolls WHERE final = true")
+x = cur.fetchall()
+if len(x) == 0:
+    con.close()
+    quit()
+try:
+
+    allDetails = []
+    allRolls = cur.execute("SELECT * FROM rolls").fetchall()
+    for student in allRolls:
+        x = getStudentDetails(student)
+        allDetails.append(x)
 
 
-table = tabulate(allDetails, headers=['Name', 'Homeroom', 'Status'], tablefmt='grid')
+    table = tabulate(allDetails, headers=['Name', 'Homeroom', 'Status'], tablefmt='grid')
 
-now = datetime.now().strftime("%d-%m-%Y, %H:%M")
+    now = datetime.now().strftime("%d-%m-%Y, %H:%M")
 
-sendEmail(f"""Subject: {now}
-{table}""")
+    sendEmail(f"""Subject: {now}
+    {table}""")
+except Exception as e: 
+    sendEmail(f"""Subject: Error while trying to send email \n
+    {e}""")
