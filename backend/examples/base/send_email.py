963310fb6d32cab44901d23bc984dde4623147b3
Austin <105258719+Austin-540@users.noreply.github.com>
2023-07-08 11:38:56 +1200

    update send_email.py

--- backend/examples/base/send_email.py
+++ backend/examples/base/send_email.py
@@ -6,10 +6,24 @@ import smtplib, ssl
 from tabulate import tabulate
 from datetime import datetime
 
+def sendEmail(contents):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(secrets['my_email'], secrets['my_password'])
+        server.sendmail(secrets['my_email'], secrets['reciever_email'], contents)
+
+    #cur.execute("DELETE FROM rolls")
+    con.commit()
+    con.close()
+
+def getStudentDetails(student):
+    studentDBDetails = cur.execute(f"SELECT name, homeroom FROM students WHERE id = '{student[4]}'").fetchone()
+    return [studentDBDetails[0], studentDBDetails[1], student[3]]
+
 
 secrets = getSecrets()
 
-con = sqlite3.connect("/Users/austin/Programming/music_lessons_attendance/backend/examples/base/pb_data/data.db")
+con = sqlite3.connect("/home/austin/helloworld/pb_data/data.db")
 cur = con.cursor()
 
 cur.execute("SELECT * FROM rolls WHERE final = true")
@@ -17,38 +31,21 @@ x = cur.fetchall()
 if len(x) == 0:
     con.close()
     quit()
+try:
 
-def sendEmail(contents):
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(secrets['my_email'], secrets['my_password'])
-        server.sendmail(secrets['my_email'], secrets['reciever_email'], contents)
-
-    cur.execute("DELETE FROM rolls")
-    con.commit()
-    con.close()
-
-def getStudentDetails(student):
-    if student[3] == 1:
-        status = "Present"
-    else:
-        status = "Absent"
-
-    studentDBDetails = cur.execute(f"SELECT name, homeroom FROM students WHERE id = '{student[4]}'").fetchone()
-    return [studentDBDetails[0], studentDBDetails[1], status]
-
-    
-
-allDetails = []
-allRolls = cur.execute("SELECT * FROM rolls").fetchall()
-for student in allRolls:
-    x = getStudentDetails(student)
-    allDetails.append(x)
+    allDetails = []
+    allRolls = cur.execute("SELECT * FROM rolls").fetchall()
+    for student in allRolls:
+        x = getStudentDetails(student)
+        allDetails.append(x)
 
 
-table = tabulate(allDetails, headers=['Name', 'Homeroom', 'Status'], tablefmt='grid')
+    table = tabulate(allDetails, headers=['Name', 'Homeroom', 'Status'], tablefmt='grid')
 
-now = datetime.now().strftime("%d-%m-%Y, %H:%M")
+    now = datetime.now().strftime("%d-%m-%Y, %H:%M")
 
-sendEmail(f"""Subject: {now}
-{table}""")
+    sendEmail(f"""Subject: {now}\n
+    {table}""")
+except Exception as e: 
+    sendEmail(f"""Subject: Error while trying to send email \n
+    {e}""")
