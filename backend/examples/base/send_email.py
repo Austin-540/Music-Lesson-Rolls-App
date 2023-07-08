3aed542146aff8ad37e5f899e5019803a97a4b64
Austin <105258719+Austin-540@users.noreply.github.com>
2023-07-08 18:20:29 +1200

    More details on email

--- backend/examples/base/send_email.py
+++ backend/examples/base/send_email.py
@@ -5,6 +5,7 @@ from secrets import getSecrets
 import smtplib, ssl
 from tabulate import tabulate
 from datetime import datetime
+from datetime import date
 
 def sendEmail(contents):
     context = ssl.create_default_context()
@@ -22,6 +23,20 @@ def getStudentDetails(student):
     return [studentDBDetails[0], studentDBDetails[1], student[6]]
 
 
+def getLessonTime(student):
+    cur.execute(f"SELECT time FROM lessons WHERE id = '{student[2]}'")
+    time = cur.fetchone()
+
+    date_last_marked = cur.execute(f"SELECT date_last_marked FROM lessons WHERE id = '{student[2]}'").fetchone()
+    print(date_last_marked)
+    print(date.today().strftime("%d_%m"))
+    if f"{int(date.today().strftime('%d'))}_{int(date.today().strftime('%m'))}" == date_last_marked[0]:
+        marked_today = True
+    else:
+        marked_today = False
+    return [datetime.strptime(time[0], "%H%M").strftime("%I:%M"), marked_today]
+
+
 secrets = getSecrets()
 
 con = sqlite3.connect("/home/austin/helloworld/pb_data/data.db")
@@ -31,22 +46,29 @@ cur.execute("SELECT * FROM rolls WHERE final = true")
 x = cur.fetchall()
 if len(x) == 0:
     con.close()
+    print("quitting - no final found")
     quit()
-try:
-
-    allDetails = []
-    allRolls = cur.execute("SELECT * FROM rolls").fetchall()
-    for student in allRolls:
-        x = getStudentDetails(student)
-        allDetails.append(x)
 
 
-    table = tabulate(allDetails, headers=['Name', 'Homeroom', 'Status'], tablefmt='grid')
+allDetails = []
+allRolls = cur.execute("SELECT * FROM rolls").fetchall()
+for student in allRolls:
+	x = getStudentDetails(student)
+	allDetails.append(x)
+	time = getLessonTime(student)
 
-    now = datetime.now().strftime("%d-%m-%Y, %H:%M")
 
-    sendEmail(f"""Subject: {now}\n
-    {table}""")
-except Exception as e: 
-    sendEmail(f"""Subject: Error while trying to send email \n
-    {e}""")
+if time[1] == True:
+    already_marked = "!!  This lesson has already been marked -- This is the updated information."
+else:
+    already_marked = ""
+
+table = tabulate(allDetails, headers=['Name', 'Homeroom', 'Status'], tablefmt='grid')
+
+now = datetime.now().strftime("%d-%m-%Y, %H:%M")
+
+sendEmail(f"""Subject: {now}\n
+    {table}
+
+Lesson start time: {time[0]}
+{already_marked}""")
