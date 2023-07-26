5a7bcb5e6d13e7908f51e69249b01ca3c58a18a6
Austin <105258719+Austin-540@users.noreply.github.com>
2023-07-26 22:59:14 +1200

    google sheets stuff

--- backend/examples/base/google_sheets.py
+++ backend/examples/base/google_sheets.py
@@ -22,29 +22,33 @@ def mark_roll(wksheet, name, status):
 
 
 
-sa = gspread.service_account(filename="/Users/austin/Downloads/google_sheets/client_secret.json")
-sheet = sa.open("App - SHC Music Lessons 2023")
+try:
+
+    sa = gspread.service_account(filename="/Users/austin/Downloads/google_sheets/client_secret.json")
+    sheet = sa.open("App - SHC Music Lessons 2023")
 
 
 
 
-con = sqlite3.connect("/Users/austin/Programming/music_lessons_attendance/backend/examples/base/pb_data/data.db")
-cur = con.cursor()
+    con = sqlite3.connect("/Users/austin/Programming/music_lessons_attendance/backend/examples/base/pb_data/data.db")
+    cur = con.cursor()
 
-all_students = cur.execute("SELECT * FROM rolls").fetchall()
+    all_students = cur.execute("SELECT * FROM rolls").fetchall()
 
-teacher_id = cur.execute("SELECT teacher FROM lessons WHERE id = ?",[all_students[0][2]]).fetchone()[0]
+    teacher_id = cur.execute("SELECT teacher FROM lessons WHERE id = ?",[all_students[0][2]]).fetchone()[0]
 
-teacher_name = cur.execute("SELECT username FROM users WHERE id = ?", [teacher_id]).fetchone()[0]
+    teacher_name = cur.execute("SELECT username FROM users WHERE id = ?", [teacher_id]).fetchone()[0]
 
 
-wksheet = sheet.worksheet(teacher_name)
+    wksheet = sheet.worksheet(teacher_name)
 
-for student in all_students:
-    try:
-        name = cur.execute("SELECT name FROM students WHERE id = ?", [student[3]]).fetchone()[0]
-        status = student[6]
-        mark_roll(wksheet, name, status)
-    except:
-        mark_roll(sheet.worksheet(f"{teacher_name}2"), name, status)
+    for student in all_students:
+        try:
+            name = cur.execute("SELECT name FROM students WHERE id = ?", [student[3]]).fetchone()[0]
+            status = student[6]
+            mark_roll(wksheet, name, status)
+        except:
+            mark_roll(sheet.worksheet(f"{teacher_name}2"), name, status)
 
+except:
+    print("Something went wrong with the google sheets")
