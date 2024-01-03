e0be5995a23dff8b9fe422852f6d1b2ad8edbe28
Austin <105258719+Austin-540@users.noreply.github.com>
2024-01-03 22:25:14 +1300

    Fix lack of padding on clear_db_page

--- lib/clear_db_page.dart
+++ lib/clear_db_page.dart
@@ -34,76 +34,84 @@ class _ClearDBPageState extends State<ClearDBPage> {
   }
   @override
   Widget build(BuildContext context) {
+    double paddingWidth = 8;
+    if (MediaQuery.of(context).size.width > 550) {
+      paddingWidth = (MediaQuery.of(context).size.width - 550) / 2;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Clear the DB"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: ListView(
-        children: [Column(children: [
-          Card(
-            child: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Icon(Icons.info),
-                  Flexible(child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text("Sometimes the backend could get clogged up. This could have been caused by someone's phone disconnecting from the internet halfway through uploading a lesson, or a glitch, or the server may have crashed while uploading a lesson. Either way, the easiest way to fix it is to delete the data and try again."),
-                  )),
-                ],
-              ),
-            )),
-            FutureBuilder(future: getDataInDB(), 
-            builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.hasData){
-
-              
-              if (snapshot.data[0].isEmpty) {
-                return Text("Nothing to show", style: TextStyle(fontSize: 25),);
-              } else {
-
-              
-              return Column(
-                children: [
-                  Text("You are about to delete this data:"),
-                  for (int x=0; x< snapshot.data[0].length; x++) ... [
-                  Card(
-                    color: Color.fromARGB(255, 251, 142, 142),
-                    child: Row(
-                    children: [
-                      Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Text("Student name: ${snapshot.data[0][x]["expand"]["students"]["name"].toString()}", 
-                            style: TextStyle(fontSize: 20),),
-                            Text("Uploaded by: ${snapshot.data[1][x].toString()}")
-                          ],
-                          
-                        ),
-                      ),
-                      Spacer()
-                    ],
-                  ),)
+        children: [Padding(
+          padding: EdgeInsets.symmetric(horizontal: paddingWidth, vertical: 10),
+          child: Column(children: [
+            Card(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.info),
+                    Flexible(child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text("Sometimes the backend could get clogged up. This could have been caused by someone's phone disconnecting from the internet halfway through uploading a lesson, or a glitch, or the server may have crashed while uploading a lesson. Either way, the easiest way to fix it is to delete the data and try again."),
+                    )),
                   ],
-                  ElevatedButton.icon(onPressed: () =>
-                  showDialog(context: context, builder:  (BuildContext context) => AlertDialog(
-                    title: Text("Confirm"),
-                    content: Text("This data cannot be recovered. This roll will need to be remarked in the more options menu. It will not reappear on the home screen."),
-                    actions: [
-                      TextButton(onPressed: () => Navigator.pop(context), child: Text("Cancel"),),
-                      TextButton(onPressed: () => Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> DeleteStuffPage()), (route) => false), child: Text("I'm sure"))
+                ),
+              )),
+              FutureBuilder(future: getDataInDB(), 
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData){
+        
+                
+                if (snapshot.data[0].isEmpty) {
+                  return Text("Nothing to show", style: TextStyle(fontSize: 25),);
+                } else {
+        
+                
+                return Column(
+                  children: [
+                    Text("You are about to delete this data:"),
+                    for (int x=0; x< snapshot.data[0].length; x++) ... [
+                    Card(
+                      color: Color.fromARGB(255, 251, 142, 142),
+                      child: Row(
+                      children: [
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Text("Student name: ${snapshot.data[0][x]["expand"]["students"]["name"].toString()}", 
+                              style: TextStyle(fontSize: 20),),
+                              Text("Uploaded by: ${snapshot.data[1][x].toString()}")
+                            ],
+                            
+                          ),
+                        ),
+                        Spacer()
+                      ],
+                    ),)
                     ],
-                  ),)
-                  , icon: Icon(Icons.delete_forever_outlined), label: Text("Delete this data"))
-                ],
-              );
-            }} else {
-              return Center(child: CircularProgressIndicator());
-            }} )
-        ]),]
+                    ElevatedButton.icon(onPressed: () =>
+                    showDialog(context: context, builder:  (BuildContext context) => AlertDialog(
+                      title: Text("Confirm"),
+                      content: Text("This data cannot be recovered. This roll will need to be remarked in the more options menu. It will not reappear on the home screen."),
+                      actions: [
+                        TextButton(onPressed: () => Navigator.pop(context), child: Text("Cancel"),),
+                        TextButton(onPressed: () => Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> DeleteStuffPage()), (route) => false), child: Text("I'm sure"))
+                      ],
+                    ),)
+                    , icon: Icon(Icons.delete_forever_outlined), label: Text("Delete this data"))
+                  ],
+                );
+              }} else {
+                return Center(child: CircularProgressIndicator());
+              }} )
+          ]),
+        ),]
       ),
     );
   }
