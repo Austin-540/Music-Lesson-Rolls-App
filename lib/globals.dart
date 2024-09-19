8cd8fd89360e391e5710fb19112faac51df53741
Austin <105258719+Austin-540@users.noreply.github.com>
2024-09-19 18:38:17 +1200

    v6.1.3 v-bump

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,7 +2,7 @@ import 'package:pocketbase/pocketbase.dart';
 
 final pb = PocketBase('https://app.shcmusiclessonrolls.com/');
 
-const version = "v6.1.2";
+const version = "v6.1.3";
 //Also change this value in pubspec.yaml
 //If I knew that the proper way to do this was in pubspec.yaml i would have done that
 //But now its difficult to retroactively fix
