c68d8175fbf491b5f7611dcc2de3dcaba869634a
Austin <105258719+Austin-540@users.noreply.github.com>
2026-03-11 14:43:34 +1300

    v6.2.1

--- lib/one_off_lesson_page.dart
+++ lib/one_off_lesson_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pocketbase/pocketbase.dart';
 import 'globals.dart';
 import 'home_page.dart';
 
@@ -173,27 +174,16 @@ class _OneOffLessonSubmitPageState extends State<OneOffLessonSubmitPage> {
         "student_names":formattedStudentNamesString,
         "time": widget.time,
       };
-      await pb
+      try {
+        await pb
           .collection('one_off_rolls')
           .create(body: body); //create a record with each students' info
-
-    await Future.delayed(const Duration(milliseconds: 200));
-      final dataInDB = await pb.collection('one_off_rolls').getFullList(
-  sort: '-created',
-);
-      if (dataInDB.isNotEmpty) {
-        if (!mounted) return;
-        for (var record in dataInDB) {
-          await pb.collection('one_off_rolls').delete(record.id);
+      } on ClientException catch (e) {
+        if (e.originalError is TypeError) {
+          return "Success - but with a type error";
         }
-        if (!mounted) return;
-        showDialog(context: context, 
-        builder: (context) => const AlertDialog(
-          title: Text("Something went wrong."),
-          content: Text("The server accepted the lesson you submitted, but wasn't able to send the email."),
-          icon: Icon(Icons.info_outline),
-        ));
       }
+      
     return "Success"; //finish FutureBuilder
   }
 
